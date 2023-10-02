160d49ca6321d576c9b492239ec0af3fcd0369e2
devxavierr <devxavierr@gmail.com>
2023-10-02 15:32:04 -0300

    inicia o projeto

---
+++ src/test/java/br/com/attornatus/pessoaapi/PessoaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.pessoaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
