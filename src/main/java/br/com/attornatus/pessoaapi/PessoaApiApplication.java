1f5a564bb68c36d5966556e0ef7a22425841ab59
devxavierr <devxavierr@gmail.com>
2023-10-02 16:40:30 -0300

    implementa api de teste

--- src/main/java/br/com/attornatus/pessoaapi/PessoaApiApplication.java
+++ src/main/java/br/com/attornatus/pessoaapi/PessoaApiApplication.java
@@ -2,9 +2,19 @@ package br.com.attornatus.pessoaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class PessoaApiApplication {
+	
+	@GetMapping
+	public String getHomeTeste() {
+		return "Teste da API OK!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PessoaApiApplication.class, args);
