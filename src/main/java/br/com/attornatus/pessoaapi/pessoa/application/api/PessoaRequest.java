52cfe3541a5cb46650ea73a1f761c1f95f4e0f54
devxavierr <devxavierr@gmail.com>
2023-10-02 20:01:12 -0300

    implementa request e response

--- src/main/java/br/com/attornatus/pessoaapi/pessoa/application/api/PessoaRequest.java
+++ src/main/java/br/com/attornatus/pessoaapi/pessoa/application/api/PessoaRequest.java
@@ -1,5 +1,16 @@
 package br.com.attornatus.pessoaapi.pessoa.application.api;
 
-public class PessoaRequest {
+import java.time.LocalDate;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.Value;
+
+@Value
+public class PessoaRequest {
+	@NotBlank
+	private String nomeCompleto;
+	@NotNull
+	private LocalDate dataNascimento;
 }
--- src/main/java/br/com/attornatus/pessoaapi/pessoa/application/api/PessoaResponse.java
+++ src/main/java/br/com/attornatus/pessoaapi/pessoa/application/api/PessoaResponse.java
@@ -1,5 +1,10 @@
 package br.com.attornatus.pessoaapi.pessoa.application.api;
 
-public class PessoaResponse {
+import java.util.UUID;
 
+import lombok.Value;
+
+@Value
+public class PessoaResponse {
+	private UUID idPessoa;
 }
